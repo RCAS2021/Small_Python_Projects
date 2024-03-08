43b97a22e4471eaa0141f1ffc44b130d94ba7765
Rafael Cruz <rcas2009@outlook.com>
2024-03-08 13:07:31 -0300

    Starting a password manager

---
+++ password_manager.py
@@ -0,0 +1,27 @@
+""" ENCRYPT AND SAVE PASSWORD ON TEXT FILE """
+from os import sys
+master_password = input("Type your master password: ")
+
+def view():
+    pass
+
+def add():
+    name = input("Account name: ")
+    pwd = input("Password: ")
+
+    with open("passwords.txt", "a") as f:
+        f.write(name + "|" + pwd)
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, add)? ").lower()
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode")
+        continue
+
+    stop = input("Quit? Y/N ").lower()
+    if stop != "n":
+        sys.exit()
