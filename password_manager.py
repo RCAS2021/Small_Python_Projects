ed1c5f62df651edb9a8ac4bae1ea76479c560bf7
Rafael Cruz <rcas2009@outlook.com>
2024-03-08 13:11:15 -0300

    create view function

--- password_manager.py
+++ password_manager.py
@@ -3,14 +3,18 @@ from os import sys
 master_password = input("Type your master password: ")
 
 def view():
-    pass
+    """ FUNCTION FOR VIEWING PASSWORDS FROM TEXT FILES """
+    with open("passwords.txt", "r", encoding="utf8") as f:
+        for i in f.readlines():
+            print(i)
 
 def add():
+    """ FUNCTION FOR ADDING NAME/PASSWORDS IN TEXT FILE"""
     name = input("Account name: ")
     pwd = input("Password: ")
 
     with open("passwords.txt", "a", encoding="utf8") as f:
-        f.write(name + "|" + pwd)
+        f.write(name + "|" + pwd + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add)? ").lower()
