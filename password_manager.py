0bfd64205fedc70ee13bede8e00abc5e87cad35a
Rafael Cruz <rcas2009@outlook.com>
2024-03-08 21:57:00 -0300

    Add function for adding master and check for file existence

--- password_manager.py
+++ password_manager.py
@@ -1,5 +1,6 @@
 """ ENCRYPT AND SAVE PASSWORD ON TEXT FILE """
 from os import sys
+import os
 from werkzeug.security import generate_password_hash, check_password_hash
 import re
 
@@ -23,20 +24,38 @@ def add():
     with open("passwords.txt", "a", encoding="utf8") as f:
         f.write(name + "|" + pwd + "\n")
 
-while True:
-    with open("passwords.txt", "r", encoding="utf8") as f:
-        master = f.readline().rstrip().split("|")
-    if check_password_hash(master[1], master_password):
-        mode = input("Would you like to add a new password or view existing ones (view, add, quit)? ").lower()
-        if mode == "view":
-            view()
-        elif mode == "add":
-            add()
-        elif mode == "quit":
-            sys.exit()
+
+def add_master():
+    """ FUNCTION FOR ADDING MASTER - RUN FIRST AND ONLY ONE TIME PER TEXT FILE """
+    print("Password file not found, create master account")
+    name = input("Account name: ")
+    pwd = input("Password: ")
+    pwd = generate_password_hash(pwd, method="scrypt")
+    with open("passwords.txt", "a", encoding="utf8") as f:
+        f.write(name + "|" + pwd + "\n")
+
+
+# RUN IF TEXT FILE DOESN'T EXIST
+if  os.path.exists("passwords.txt"):
+
+    while True:
+        with open("passwords.txt", "r", encoding="utf8") as f:
+            master = f.readline().rstrip().split("|")
+        if check_password_hash(master[1], master_password):
+            mode = input("Would you like to add a new password or view existing ones (view, add, quit)? ").lower()
+            if mode == "view":
+                view()
+            elif mode == "add":
+                add()
+            elif mode == "quit":
+                sys.exit()
+            else:
+                print("Invalid mode")
+                continue
         else:
-            print("Invalid mode")
-            continue
-    else:
-        print("Incorrect Master Password, exiting")
-        sys.exit()
+            print("Incorrect Master Password, exiting")
+            sys.exit()
+
+# RUN IF TEXT FILE DOESN'T EXIST
+else:
+    add_master()
