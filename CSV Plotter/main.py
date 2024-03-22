9c0b432e2c044e0166d5471dbdf2843f06877939
Rafael Cruz <rcas2009@outlook.com>
2024-03-22 15:49:58 -0300

    Adding commentary

--- CSV Plotter/main.py
+++ CSV Plotter/main.py
@@ -8,6 +8,8 @@ import numpy as np
 class CSVPlotter:
     def __init__(self, root):
         self.root = root
+
+        # Setting title
         root.title("CSV Plotter")
 
         # Creating a master frame
@@ -57,9 +59,11 @@ class CSVPlotter:
         exit_button.pack(side="bottom", padx=10, pady=10)
 
     def close_app(self):
+        """ Function for closing app when clicking exit button """
         self.root.quit()
 
     def load_csv(self):
+        """ Function for loading CSV File and setting attribute options when clicking the load button """
         file_path = filedialog.askopenfilename()
         if file_path:
             self.df = pd.read_csv(file_path)
@@ -70,10 +74,15 @@ class CSVPlotter:
             self.update_plot()
 
     def plot_pie(self, columns, selected_attribute):
+        """ Function for plotting the pie chart """
+        # Sets labels to be the first value on the df columns
         labels = self.df[columns[0]]
+        # Checks if the selected attribute = value in attribute example: ["Qtt", "Avg_Age"] <- in this case, attribute[0] = "Qtt"
         if selected_attribute == self.attribute[0]:
+            # If yes, plot the chart with value "Qtt"
             data = self.df[columns[1]]
         else:
+            # Else, plot the chart with value "Avg_Age"
             data = self.df[columns[2]]
         
         # Finding index of max value
@@ -84,26 +93,30 @@ class CSVPlotter:
         # Explode the index with the max value
         explode[max_index] = 0.04
 
+        # Plot the chart passing the parameters
         self.ax.pie(data, labels=labels, autopct='%1.1f%%', shadow=True, explode=explode, startangle=90, labeldistance=0.45, pctdistance=0.63, textprops={'horizontalalignment': 'center', 'verticalalignment': 'center'}, wedgeprops={'linewidth': 1}, radius=0.5, center=(0.5, 0.5))
         self.ax.set_xlabel(columns[0])  # Set x-label based on the selected attribute
         self.ax.set_ylabel(selected_attribute)  # Set y-label based on the selected attribute
+        # Sets aspect ratio to be equal along x and y axis
         self.ax.axis("equal")
 
     def create_attribute_menu(self):
+        """ Function for creating the attribute menu """
         # Creating attribute menu
         self.attribute_menu = ctk.CTkOptionMenu(master=self.frame, values=[*self.attribute_options], variable=self.attribute_var, command=self.update_plot)
         self.attribute_menu.pack(after=self.load_button, padx=10, pady=10)
 
-    def plot_bar(self, columns):
-        # Recreate the ax2 object dynamically
-        self.ax2 = self.ax.twinx()
+    def plot_bar(self):
+        """ Function for plotting the bar chart"""
+        self.ax2 = self.ax.twinx() # Create a secondary y-axis
         # Plotting first dataset on y-axis
         self.df.iloc[:,1].plot(kind="bar", color="red", ax=self.ax, width=0.4, position=1)
 
         # Plotting second dataset on second y-axis
         self.df.iloc[:,2].plot(kind="bar", color="blue", ax=self.ax2, width=0.4, position=0) 
 
-    def plot_line(self, columns):
+    def plot_line(self):
+        """ Function for plotting the line chart """
         self.ax2 = self.ax.twinx()  # Create a secondary y-axis
         # Plotting first dataset on y-axis
         self.df.iloc[:,1].plot(kind="line", color="red", ax=self.ax)
@@ -112,6 +125,7 @@ class CSVPlotter:
         self.df.iloc[:,2].plot(kind="line", color="blue", ax=self.ax2)
 
     def plot_scatter(self, columns):
+        """ Function for plotting the scatter chart """
         self.ax2 = self.ax.twinx()  # Create a secondary y-axis
         # Plotting first dataset on y-axis
         self.df.plot(kind="scatter", x=self.df.columns[0], y=self.df.columns[1], label=f"{columns[1]}", color="red", ax=self.ax)
@@ -120,11 +134,14 @@ class CSVPlotter:
         self.df.plot(kind="scatter", x=self.df.columns[0], y=self.df.columns[2], label=f"{columns[2]}", color="blue", ax=self.ax)
 
     def set_labels(self):
+        """ Function for setting the x and y labels for both axes """
         self.ax.set_xlabel(self.df.columns[0])
         self.ax.set_ylabel(self.df.columns[1])
         self.ax2.set_ylabel(self.df.columns[2])
 
     def synchronize_y_axes(self, df, y_columns):
+        """ Function for synchronizing the y axis in both axes,
+            now, for example, when there is one y axis = 50 and another = 35, the chart will represent it correctly."""
         # Calculate the maximum y value from the specified columns in the DataFrame
         max_y = max(df[y_columns].max())
 
@@ -133,10 +150,12 @@ class CSVPlotter:
         self.ax2.set_ylim(0, max_y + 10)
 
     def set_x_axis_labels(self, x_values, x_labels):
+        """ Function for setting x axis labels, on line and bar plots, it defaulted to indexes instead of labels """
         self.ax.set_xticks(x_values)  # Set x-ticks at each index
         self.ax.set_xticklabels(x_labels, rotation=0)  # Set x-tick labels to animal names
 
     def create_combined_legends(self):
+        """ Function for creating combined legends, instead of having two separate legends """
         # Creating combined legends
         # Get handles and labels for both axes
         handles1, labels1 = self.ax.get_legend_handles_labels()
@@ -150,7 +169,9 @@ class CSVPlotter:
         self.ax.legend(all_handles, all_labels, loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=2)
 
     def update_plot(self, event=None):
+        """ Function called when changing chart on menu, updates the canvas """
         if self.df is not None:
+            # Get Plot Type
             plot_type = self.plot_type_var.get()
 
             # Clear the figure and all axes
@@ -163,10 +184,11 @@ class CSVPlotter:
             if hasattr(self, 'attribute_menu') and self.attribute_menu.winfo_exists():
                 self.attribute_menu.destroy()
 
+            # Selecting the plot type            
             if plot_type == "Line Plot":
-                self.plot_line(list(self.df.columns))
+                self.plot_line()
             elif plot_type == "Bar Plot":
-                self.plot_bar(list(self.df.columns))
+                self.plot_bar()
             elif plot_type == "Scatter Plot":
                 self.plot_scatter(list(self.df.columns))
             elif plot_type == "Pie":
@@ -174,15 +196,17 @@ class CSVPlotter:
                 self.attribute_options = list(self.df.columns[1:])
                 self.create_attribute_menu()
                 selected_attribute = self.attribute_var.get()  # Get the selected attribute
-                self.plot_pie(list(self.df.columns), selected_attribute)  # Pass the selected attribute)
+                self.plot_pie(list(self.df.columns), selected_attribute)  # Pass the selected attribute
 
+            # Pie chart works differently, so can't use the same methods, therefore it is excluded
             if plot_type != "Pie":
                 # Setting title
                 self.ax.set_title("Animals X Quantity X Average_Age")
 
+                # Synchronizes y axes
                 self.synchronize_y_axes(self.df, self.df.columns[1:])
                 # Set x-axis labels
-                x_values = range(len(self.df))
+                x_values = range(len(self.df)) # Gets x values
                 self.set_x_axis_labels(x_values, self.df[self.df.columns[0]])
                 
                 # Setting labels
@@ -198,12 +222,19 @@ class CSVPlotter:
             self.canvas.draw()
 
     def show_statistics(self):
+        """ Function for inserting text in a textbox, used for any text statistic displaying """
+        # Reactivates textbox
         self.textbox.configure(state="normal")
+        # Deletes content inside textbox
         self.textbox.delete(1.0, "end")
+        # Getting the animal with most quantity
         most_animal = self.df.loc[self.df.Qtt == max(self.df.Qtt)]
+        # Getting animal type, for example: Dog
         most_animal = str(most_animal['Animals'].values).strip('[]').strip("'")
+        # Inserting f-string in textbox
         self.textbox.insert("0.0", f"The biggest average age is: {max(self.df.Avg_Age)}\nThe animal type with most animals is: {most_animal} with {max(self.df.Qtt)} total\n" +
                             f"There are {sum(self.df.Qtt)} animals in total\nThe average of all average ages is: {np.mean(self.df.Avg_Age)}")
+        # Deactivating textbox to be un-writable
         self.textbox.configure(state="disabled")
 
 if __name__ == "__main__":
