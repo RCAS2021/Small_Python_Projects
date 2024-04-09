50c0825184f6d8d2af937110945570cd9f27bac0
Rafael Cruz <rcas2009@outlook.com>
2024-04-09 15:02:43 -0300

    Creating line plot example

--- Better Plotter/main.py
+++ Better Plotter/main.py
@@ -2,12 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Creating some data
-X_data = np.random.random(50) * 100
-Y_data = np.random.random(50) * 100
+#X_data = np.random.random(50) * 100
+#Y_data = np.random.random(50) * 100
 
 # Creating scatter plot with some parameters
-plt.scatter(X_data, Y_data, marker="*", s=75, alpha=0.3)
+#plt.scatter(X_data, Y_data, marker="*", s=75, alpha=0.3)
 
+# Preparing data for line plot
+years = [2006 + x for x in range(16)]
+weights = [80, 83, 84, 85, 86, 82, 81, 79, 83, 81, 80, 76 , 75, 80, 79, 80]
+
+# Creating line plot with some parameters
+plt.plot(years, weights)
 
 
 
