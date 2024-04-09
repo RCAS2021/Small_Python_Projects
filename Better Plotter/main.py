90578bb03e7e67ddf9f27631b5e472daf7baec30
Rafael Cruz <rcas2009@outlook.com>
2024-04-09 15:03:04 -0300

    Creating bar plot example

--- Better Plotter/main.py
+++ Better Plotter/main.py
@@ -9,13 +9,18 @@ import matplotlib.pyplot as plt
 #plt.scatter(X_data, Y_data, marker="*", s=75, alpha=0.3)
 
 # Preparing data for line plot
-years = [2006 + x for x in range(16)]
-weights = [80, 83, 84, 85, 86, 82, 81, 79, 83, 81, 80, 76 , 75, 80, 79, 80]
+#years = [2006 + x for x in range(16)]
+#weights = [80, 83, 84, 85, 86, 82, 81, 79, 83, 81, 80, 76 , 75, 80, 79, 80]
 
 # Creating line plot with some parameters
-plt.plot(years, weights)
+#plt.plot(years, weights)
 
+# Creating data for bar plot
+x = ["C++", "C#", "Python", "Java", "Go"]
+y = [20, 50, 140, 3, 45]
 
+# Creating bar plot with some parameters
+plt.bar(x, y)
 
 
 plt.show()
