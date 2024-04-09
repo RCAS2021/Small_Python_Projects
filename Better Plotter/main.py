687feff7b6c1622c1c6280369ee33e093a14124c
Rafael Cruz <rcas2009@outlook.com>
2024-04-09 15:02:17 -0300

    Creating scatter plot example

---
+++ Better Plotter/main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Creating some data
+X_data = np.random.random(50) * 100
+Y_data = np.random.random(50) * 100
+
+# Creating scatter plot with some parameters
+plt.scatter(X_data, Y_data, marker="*", s=75, alpha=0.3)
+
+
+
+
+
+plt.show()
+
+
+
+
+
+
