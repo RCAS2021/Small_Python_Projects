a86de6437263052ef6bda073c4e75577e1ebe2f1
Rafael Cruz <rcas2009@outlook.com>
2024-04-16 10:13:40 -0300

    Setting up tiles

--- 2048/main.py
+++ 2048/main.py
@@ -26,6 +26,55 @@ FONT = pygame.font.SysFont("comicsans", 60, bold=True)
 # Velocity of rectangles movement
 MOVE_VELOCITY = 20
 
+class Tile:
+    COLORS = [
+        (237, 229, 218),
+        (238, 225, 201),
+        (246, 150, 101),
+        (247, 124, 95),
+        (247, 95, 59),
+        (237, 208, 115),
+        (237, 204, 99),
+        (236, 202, 80),
+    ]
+
+    def __init__(self, value, row, col):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.x = col * RECT_WIDTH
+        self.y = row * RECT_HEIGHT
+
+    def get_color(self):
+        # Getting what power of 2 the tile value is and subtract 1
+        color_index = int(math.log2(self.value) - 1)
+        # Select color based on color index
+        color = self.COLORS[color_index]
+        return color
+
+    def draw_tile(self, window):
+        color = self.get_color()
+        pygame.draw.rect(window, color, (self.x, self.y, RECT_WIDTH, RECT_HEIGHT))
+
+        # Creating a surface for the text
+        text = FONT.render(str(self.value), 1, FONT_COLOR)
+        # Put a surface on the screen
+        window.blit(
+            text,
+            (
+                self.x + (RECT_WIDTH / 2 - text.get_width() / 2),
+                self.y + (RECT_HEIGHT / 2 - text.get_height() / 2),
+            ),
+        )
+
+    def set_pos(self):
+        pass
+
+    def move(self, delta):
+        pass
+
+
+
 def draw_grid(window):
     # Drawing horizontal lines
     for row in range(1, ROWS):
@@ -41,18 +90,26 @@ def draw_grid(window):
     # Drawing border
     pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
 
-def draw(window):
+def draw(window, tiles):
     # Changing background color
     window.fill(BACKGROUND_COLOR)
+
+    for tile in tiles.values():
+        tile.draw_tile(window)
 
     draw_grid(window)
 
     pygame.display.update()
 
+
+
+
 def main(window):
     # Setting game speed
     clock = pygame.time.Clock()
     run = True
+
+    tiles = {}
 
     while run:
         clock.tick(FPS)
@@ -63,7 +120,7 @@ def main(window):
                 run = False
                 break
 
-        draw(window)
+        draw(window, tiles)
 
 
     pygame.quit()
