2c0571b4bdd97216564c1096ead0b632c5be1ee9
Rafael Cruz <rcas2009@outlook.com>
2024-04-16 09:42:06 -0300

    Importing pygame, setting constants

---
+++ 2048/main.py
@@ -0,0 +1,29 @@
+import pygame
+import random
+import math
+
+# Initialize pygame
+pygame.init()
+
+# Defining constants
+FPS = 60
+WIDTH, HEIGHT = 800, 960
+ROWS = 4
+COLUMNS = 4
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLUMNS
+OUTLINE_COLOR = (187, 173, 160)
+OUTLINE_THICKNESS = 10
+BACKGROUND_COLOR = (205, 192, 180)
+FONT_COLOR = (119, 110, 101)
+
+# Creating window
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+# Setting caption
+pygame.display.set_caption("2048")
+# Setting font
+FONT = pygame.font.SysFont("comicsans", 60, bold=True)
+# Velocity of rectangles movement
+MOVE_VELOCITY = 20
+
+
